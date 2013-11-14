828f62bdc033a19c525a1ac0a3e5d16ef8bd2fa7
Eric Koo <erickoo@umich.edu>
2013-11-14 08:16:41 -0500

    Added <html> tags to login.html, added setup.sql

---
+++ setup.sql
@@ -0,0 +1,21 @@
+CREATE TABLE employer (
+	id MEDIUMINT NOT NULL AUTO_INCREMENT,
+	first VARCHAR(255),
+	last VARCHAR(255),
+	email VARCHAR(255),
+	organization VARCHAR(255),
+	username VARCHAR(255),
+	password VARCHAR(255),
+	PRIMARY KEY (id)
+ ) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+ 
+CREATE TABLE listing (
+	id MEDIUMINT NOT NULL AUTO_INCREMENT,
+	employer_id MEDIUMINT,
+	date DATE,
+	end_date DATE,
+	location VARCHAR(255),
+	link VARCHAR(255),
+	desc VARCHAR,
+	PRIMARY KEY (id)
+ ) ENGINE = InnoDB DEFAULT CHARSET=utf8;
