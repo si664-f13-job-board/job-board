f1149c45eb4896f217928c70251d94312c78a488
SungJin Nam <lucidhole@gmail.com>
2013-11-27 11:16:26 -0500

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -9,35 +9,27 @@ CREATE TABLE employer (
 	PRIMARY KEY (id)
  ) ENGINE = InnoDB DEFAULT CHARSET=utf8;
  
--- added position and salary field
-CREATE TABLE listing (
-	id MEDIUMINT NOT NULL AUTO_INCREMENT,
-	employer_id MEDIUMINT,
-<<<<<<< HEAD
-	position VARCHAR(255),
-	salary VARCHAR(255),
-=======
+ CREATE TABLE listing (
+    id MEDIUMINT NOT NULL AUTO_INCREMENT,
+    employer_id MEDIUMINT,
 	title VARCHAR(255),
 	remote TINYINT,
 	paid TINYINT,
 	hours MEDIUMINT,
->>>>>>> 8ca14504ceecfae6cb0f90ec36bfc15cf70c6075
 	date DATE,
 	end_date DATE,
+	remote TINYINT,
+	link VARCHAR(255),
 	description LONGTEXT,
 	skills LONGTEXT,
 	link VARCHAR(255),
-<<<<<<< HEAD
-	descr VARCHAR(4096),
-=======
 	email VARCHAR(255),
 	name VARCHAR(255),
 	
 	CONTRAINT `employer_ref`
-		FOREIGN KEY (`employer_id`)
-		REFERENCES `employer` (`id`),
-		
->>>>>>> 8ca14504ceecfae6cb0f90ec36bfc15cf70c6075
+	  FOREIGN KEY (`employer_id`)
+	  REFERENCES `employer` (`id`),
+	  
 	PRIMARY KEY (id)
-
- ) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+	
+   ) ENGINE = InnoDB DEFAULT CHARSET=utf8;
