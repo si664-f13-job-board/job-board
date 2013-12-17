6c4d3e394de2e0b246bc6bc9e47a1d0e42ca1aa9
Eric Koo <erickoo@umich.edu>
2013-12-16 22:40:55 -0500

    Made listings in my_listings.php stylized as well

--- my_listings.php
+++ my_listings.php
@@ -12,15 +12,15 @@
 
 <div class="container">
 	<h1>JOB LISTINGS</h1>
+	<div class="joblisting_container">
 	<?php
 		function boolToString($input){
 			if($input == 1) return('YES');
 			else return('NO');
 		}
-		
-	    $sql = "
-			SELECT listing.listing_id
-			     , listing.title
+
+	    $listing_results = $pdo->query("
+			SELECT listing.title
 			     , listing.description
 				 , listing.skills
 			     , listing.remote
@@ -35,25 +35,32 @@
 			FROM   listing
 			       JOIN employer
 				   ON listing.employer_id = employer.employer_id
-			WHERE  employer.employer_id = :employer_id";
+			WHERE  employer.employer_id = :employer_id
+			ORDER BY listing.post_date DESC";
 		$listing_results = $pdo -> prepare($sql);
 		$listing_results -> execute(array(':employer_id'=>$_SESSION['account']));
 		
 		while($row = $listing_results -> fetch(PDO::FETCH_ASSOC)){
 			echo '<div class="job_listing">';
-			echo '<a href="edit_listing.php?listing_id='.$row['listing_id'].'">Edit</a></br>';
-			echo 'EMPLOYER/COMPANY: '.$row['organization'].'</br>';
-			echo 'TITLE: '.$row['title'].'</br>';
-			echo 'REMOTE: '.boolToString($row['remote']).'</br>';
-			echo 'PAID: '.boolToString($row['paid']).'</br>';
-			echo 'HOURS: '.$row['hours'].'</br>';
-			echo 'DATE POSTED: '.$row['post_date'].'</br>';
-			echo 'DEADLINE: '.$row['end_date'].'</br>';
-			echo 'DESCRIPTION: '.$row['description'].'</br>';
-			echo 'SKILLS: '.$row['skills'].'</br>';
-			echo 'NAME: '.$row['name'].'</br>';
-			echo 'EMAIL: <a href='.$row['email'].'>'.$row['email'].'</a></br>';
-			echo 'LINK: <a href='.$row['link'].'>'.$row['link'].'</a></br>';
+			echo '<div class="employer_title">';
+			echo ''.$row['title'].'</br>';
+			echo '</div>';
+			echo '<div class="description">';
+			echo '<i>Description: </i>'.$row['description'].'</br>';
+			echo '</div>';
+			echo '<i>Remote: </i> '.boolToString($row['remote']).'</br>';
+			echo '<i> Paid: </i>'.boolToString($row['paid']).'</br>';
+			echo '<i>Hours:</i> '.$row['hours'].'</br>';
+			echo '<i>Date Posted: </i>'.$row['post_date'].'</br>';
+			echo '<i>Deadline: </i>'.$row['end_date'].'</br>';
+			echo '<div class="skills">';
+			echo '<i>Skills: </i>'.$row['skills'].'</br>';
+			echo '</div>';
+			echo '<div class="job_link">';
+			echo '<i>Contact: </i>'.$row['name'].'</br>';
+			echo '<a href='.$row['email'].'>'.$row['email'].'</a>'.'</br>';
+			echo ' <a href='.$row['link'].'>'.$row['link'].'</a>'.'</br>';
+			echo '</div>';
 			echo '</div>';
 		}
 	?>
